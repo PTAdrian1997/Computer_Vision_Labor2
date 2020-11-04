4cca4f2b9fa1d59c84289f9d99780b7e78aabdc2
PTAdrian1997 <poptifreaadrian@gmail.com>
2020-11-04 16:17:30 +0200

    Update lab1_solution2.py
    Begin the Flood Fill function

--- application/lab1_solution2.py
+++ application/lab1_solution2.py
@@ -14,7 +14,8 @@ res, inverted_binary_image = cv.threshold(grayscale_image, type=cv.THRESH_BINARY
 """
 Get the greatest value from the sub-matrix defined by the kernel and the source image
 param source_image: a numpy matrix representing the image on which the kernel must be applied
-param kernel: a numpy matrix representing the kernel that must be applied on the source image
+param kernel: a numpy matrix representing the kernel that must be applied on the source image;
+                the kernel must be a square matrix and the side must be odd.
 param center_x: the abscissa of the point on which the anchor of the kernel must be superposed
 param center_y: the ordinate of the point on which the anchor of the kernel must be superposed
 """
@@ -23,3 +24,18 @@ def apply_dilation_kernel(source_image, kernel, center_x, center_y):
     lim_y_1, lim_y_2 = center_y - int(kernel.shape[0] / 2), center_y + int(kernel.shape[0] / 2)
     source_interesting_portion = source_image[lim_y_1:lim_y_2, lim_x_1:lim_x_2] * kernel
     return source_interesting_portion.max()
+
+"""
+    
+"""
+def flood_fill(source_image, start_point_y, start_point_x):
+    kernel = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
+    initial_image = np.array(source_image)
+
+    # get the complement of the source_image:
+    grayscale_image = cv.cvtColor(source_image, cv.COLOR_BGR2GRAY)
+    res, inverted_binary_image = cv.threshold(grayscale_image, type=cv.THRESH_BINARY_INV, maxval=255, thresh=0)
+
+    # do the flood fill:
+    
+    return initial_image
